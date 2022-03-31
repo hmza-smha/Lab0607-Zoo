51d9c2eb08269c5e0f7af004ee53f0267ac6399a
HamZa Samha <smha.hmza@gmail.com>
2022-03-31 14:25:53 +0300

    Add project files.

---
+++ Lab0607-Zoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab0607_Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
